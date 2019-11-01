619bc5994eb227cd7901d69eb1f097419dc276e5
William Mian <william_mian@yahoo.com.br>
2019-11-01 15:41:39 -0300

    criando o projeto

---
+++ myfinances-api/others/schema_base_de_dados.sql
@@ -0,0 +1,26 @@
+﻿CREATE DATABASE minhasfinancas
+
+CREATE SCHEMA financas 
+
+CREATE TABLE financas.usuario
+(
+   id bigserial NOT NULL PRIMARY KEY,
+   nome character varying(150),
+   email character varying(100),
+   senha character varying(20),
+   data_cadastro date DEFAULT NOW()
+);
+
+CREATE TABLE financas.lancamento
+(
+   id bigserial NOT NULL PRIMARY KEY,
+   descricao character varying(100) NOT NULL,
+   mes integer NOT NULL,
+   ano integer NOT NULL,
+   valor numeric(16,2) NOT NULL,
+   tipo character varying(20) CHECK ( tipo in ('RECEITA', 'DESPESA')) NOT NULL,
+   status character varying(20) CHECK ( status in ('PENDENTE', 'CANCELADO', 'EFETIVADO')) NOT NULL,
+   id_usuario bigint REFERENCES financas.usuario (id) NOT NULL,
+   data_cadastro date DEFAULT NOW()
+);
+   
---
+++ myfinances-api/src/main/java/com/wm/myfinancesapi/MyfinancesApiApplication.java
@@ -0,0 +1,13 @@
+package com.wm.myfinancesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfinancesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfinancesApiApplication.class, args);
+	}
+
+}
