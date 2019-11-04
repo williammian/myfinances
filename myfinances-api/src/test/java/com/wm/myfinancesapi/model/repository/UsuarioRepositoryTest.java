3bf423f74c29db05319ea06bc2e4b81db2cdcb81
William Mian <william_mian@yahoo.com.br>
2019-11-04 10:09:40 -0300

    melhorando testes de integracao com @DataJpaTest

--- myfinances-api/src/test/java/com/wm/myfinancesapi/model/repository/UsuarioRepositoryTest.java
+++ myfinances-api/src/test/java/com/wm/myfinancesapi/model/repository/UsuarioRepositoryTest.java
@@ -4,15 +4,19 @@ import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.wm.myfinancesapi.model.entity.Usuario;
 
-@SpringBootTest
 @RunWith(SpringRunner.class)
 @ActiveProfiles("test")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 public class UsuarioRepositoryTest {
 	
 	@Autowired
