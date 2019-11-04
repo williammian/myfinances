dd62a6c2959d7ad562b5f593cf54232cbbc8f1f8
William Mian <william_mian@yahoo.com.br>
2019-11-04 13:13:10 -0300

    melhorando utilizacao do Mock no teste unitario com @MockBean

--- myfinances-api/src/test/java/com/wm/myfinancesapi/service/UsuarioServiceTest.java
+++ myfinances-api/src/test/java/com/wm/myfinancesapi/service/UsuarioServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -16,11 +17,12 @@ import com.wm.myfinancesapi.service.impl.UsuarioServiceImpl;
 public class UsuarioServiceTest {
 
 	UsuarioService service;
+	
+	@MockBean
 	UsuarioRepository repository;
 	
 	@Before
 	public void setup() {
-		repository = Mockito.mock(UsuarioRepository.class);
 		service = new UsuarioServiceImpl(repository);
 	}
 	
