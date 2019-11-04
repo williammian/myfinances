f7dffaa7bc2b3db876f9a2967664b15df41b43f4
williammian <william_mian@yahoo.com.br>
2019-11-04 07:42:58 -0200

    primeiro teste de integracao

---
+++ myfinances-api/src/test/java/com/wm/myfinancesapi/model/repository/UsuarioRepositoryTest.java
@@ -0,0 +1,32 @@
+package com.wm.myfinancesapi.model.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.wm.myfinancesapi.model.entity.Usuario;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class UsuarioRepositoryTest {
+	
+	@Autowired
+	UsuarioRepository repository;
+	
+	@Test
+	public void deveVerificarAExistenciaDeUmEmail() {
+		//cenário
+		Usuario usuario = Usuario.builder().nome("usuario").email("usuario@email.com").build();
+		repository.save(usuario);
+		
+		//ação/execução
+		boolean result = repository.existsByEmail("usuario@email.com");
+		
+		//verificação
+		Assertions.assertThat(result).isTrue();
+	}
+
+}
