9d28a9bba27541c85747cbce6199c341fb90e316
William Mian <william_mian@yahoo.com.br>
2019-11-04 13:09:47 -0300

    melhorando testes unitarios com Mocks

--- myfinances-api/src/test/java/com/wm/myfinancesapi/service/UsuarioServiceTest.java
+++ myfinances-api/src/test/java/com/wm/myfinancesapi/service/UsuarioServiceTest.java
@@ -1,43 +1,44 @@
 package com.wm.myfinancesapi.service;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.Mockito;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.wm.myfinancesapi.exception.RegraNegocioException;
-import com.wm.myfinancesapi.model.entity.Usuario;
 import com.wm.myfinancesapi.model.repository.UsuarioRepository;
+import com.wm.myfinancesapi.service.impl.UsuarioServiceImpl;
 
-@SpringBootTest
 @RunWith(SpringRunner.class)
 @ActiveProfiles("test")
 public class UsuarioServiceTest {
 
-	@Autowired
 	UsuarioService service;
-	
-	@Autowired
 	UsuarioRepository repository;
+	
+	@Before
+	public void setup() {
+		repository = Mockito.mock(UsuarioRepository.class);
+		service = new UsuarioServiceImpl(repository);
+	}
 	
 	@Test(expected = Test.None.class)
 	public void deveValidarEmail() {
-		//cenário
-		repository.deleteAll();
+		//cenario
+		Mockito.when(repository.existsByEmail(Mockito.anyString())).thenReturn(false);
 		
-		//ação
+		//acao
 		service.validarEmail("usuario@email.com");
 	}
 	
 	@Test(expected = RegraNegocioException.class)
 	public void deveLancarErroAoValidarEmailCadastrado() {
-		//cenário
-		Usuario usuario = Usuario.builder().nome("usuario").email("usuario@email.com").build();
-		repository.save(usuario);
+		//cenario
+		Mockito.when(repository.existsByEmail(Mockito.anyString())).thenReturn(true);
 		
-		//ação
+		//acao
 		service.validarEmail("usuario@email.com");
 	}
 	
