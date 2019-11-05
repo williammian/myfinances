9d4e02992345207f7358559d0f6aa2a92d77765b
William Mian <william_mian@yahoo.com.br>
2019-11-05 08:05:27 -0300

    implementando o Endpoint de autenticacao de usuarios

--- myfinances-api/src/main/java/com/wm/myfinancesapi/api/resource/UsuarioResource.java
+++ myfinances-api/src/main/java/com/wm/myfinancesapi/api/resource/UsuarioResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wm.myfinancesapi.api.dto.UsuarioDTO;
+import com.wm.myfinancesapi.exception.ErroAutenticacao;
 import com.wm.myfinancesapi.exception.RegraNegocioException;
 import com.wm.myfinancesapi.model.entity.Usuario;
 import com.wm.myfinancesapi.service.UsuarioService;
@@ -20,6 +21,16 @@ public class UsuarioResource {
 	
 	public UsuarioResource(UsuarioService service) {
 		this.service = service;
+	}
+	
+	@PostMapping("/autenticar")
+	public ResponseEntity autenticar(@RequestBody UsuarioDTO dto) {
+		try {
+			Usuario usuarioAutenticado = service.autenticar(dto.getEmail(), dto.getSenha());
+			return ResponseEntity.ok(usuarioAutenticado);
+		}catch (ErroAutenticacao e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
 	}
 	
 	@PostMapping
