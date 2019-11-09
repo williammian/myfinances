b47d756bd4791116f7ffc814cb9e5e5ad7171152
williammian <william_mian@yahoo.com.br>
2019-11-09 16:36:08 -0200

    implementando o componente de cadastro de usuarios

--- myfinances-app/src/App.js
+++ myfinances-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Login from './views/login'
+import CadastroUsuario from './views/cadastroUsuario'
 
 import 'bootswatch/dist/flatly/bootstrap.css'
 import './custom.css'
@@ -10,7 +11,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Login />
+        <CadastroUsuario />
       </div>
     )
   }
