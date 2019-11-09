c980c4bcacc40353cfaad691265175d7ffa3fea1
williammian <william_mian@yahoo.com.br>
2019-11-09 08:47:53 -0200

    adicionando estado e comportamento no componente de login

--- myfinances-app/src/views/login.js
+++ myfinances-app/src/views/login.js
@@ -4,6 +4,16 @@ import FormGroup from '../components/form-group'
 
 class Login extends React.Component {
 
+    state = {
+        email: '',
+        senha: ''
+    }
+
+    entrar = () => {
+        console.log('E-mail: ', this.state.email)
+        console.log('Senha', this.state.senha)
+    }
+
     render() {
         return(
             <div className="container">
@@ -14,16 +24,27 @@ class Login extends React.Component {
                                 <div className="row">
                                     <div className="col-lg-12">
                                         <div className="bs-component">
-                                            <fildset>
+                                            <fieldset>
                                                 <FormGroup label="Email: *" htmlFor="exampleInputEmail1">
-                                                    <input type="email" className="form-control" 
-                                                        id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Digite o Email"/>
+                                                    <input type="email" 
+                                                        value={this.state.email}
+                                                        onChange={e => this.setState({email: e.target.value})}
+                                                        className="form-control" 
+                                                        id="exampleInputEmail1" 
+                                                        aria-describedby="emailHelp" 
+                                                        placeholder="Digite o Email"/>
                                                 </FormGroup>
                                                 <FormGroup label="Senha: *" htmlFor="exampleInputPassword1">
-                                                    <input type="password" className="form-control" 
-                                                        id="exampleInputPassword1" placeholder="Password"/>
+                                                    <input type="password" 
+                                                        value={this.state.senha}
+                                                        onChange={e => this.setState({senha: e.target.value})}
+                                                        className="form-control" 
+                                                        id="exampleInputPassword1" 
+                                                        placeholder="Password"/>
                                                 </FormGroup>
-                                            </fildset>
+                                                <button onClick={this.entrar} className="btn btn-success">Entrar</button>
+                                                <button className="btn btn-danger">Cadastrar</button>
+                                            </fieldset>
                                         </div>
                                     </div>
                                 </div>
