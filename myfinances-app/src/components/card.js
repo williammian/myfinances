f9066f168522d5540a4ff82619f1cdbf6d25e50a
williammian <william_mian@yahoo.com.br>
2019-11-09 08:26:57 -0200

    componentizando o formulario de login

--- myfinances-app/src/components/card.js
+++ myfinances-app/src/components/card.js
@@ -5,7 +5,7 @@ class Card extends React.Component {
     render() {
         return(
             <div className="card mb-3">
-                <div className="card-header">{this.props.title}</div>
+                <h3 className="card-header">{this.props.title}</h3>
                 <div className="card-body">
                     {this.props.children}
                 </div>
--- myfinances-app/src/views/login.js
+++ myfinances-app/src/views/login.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Card from '../components/card'
+import FormGroup from '../components/form-group'
 
 class Login extends React.Component {
 
@@ -10,7 +11,22 @@ class Login extends React.Component {
                     <div className="col-md-6" style={ {position: 'relative', left: '300px'} }>
                         <div className="bs-docs-section">
                             <Card title="Login">
-                                Meu cartão
+                                <div className="row">
+                                    <div className="col-lg-12">
+                                        <div className="bs-component">
+                                            <fildset>
+                                                <FormGroup label="Email: *" htmlFor="exampleInputEmail1">
+                                                    <input type="email" className="form-control" 
+                                                        id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Digite o Email"/>
+                                                </FormGroup>
+                                                <FormGroup label="Senha: *" htmlFor="exampleInputPassword1">
+                                                    <input type="password" className="form-control" 
+                                                        id="exampleInputPassword1" placeholder="Password"/>
+                                                </FormGroup>
+                                            </fildset>
+                                        </div>
+                                    </div>
+                                </div>
                             </Card>
                         </div>
                     </div>
