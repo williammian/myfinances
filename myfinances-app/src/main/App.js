61858f000c55bef70620cfde517b211567178ae3
williammian <william_mian@yahoo.com.br>
2019-11-09 17:08:23 -0200

    utilizando as rotas para navegar entre as views

--- myfinances-app/src/main/App.js
+++ myfinances-app/src/main/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
 
-import Login from './views/login'
-import CadastroUsuario from './views/cadastroUsuario'
+import Rotas from './rotas'
 
 import 'bootswatch/dist/flatly/bootstrap.css'
-import './custom.css'
+import '../custom.css'
 
 class App extends React.Component {
 
   render() {
     return (
       <div>
-        <CadastroUsuario />
+        <Rotas />
       </div>
     )
   }
