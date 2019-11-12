8172b2a7f16706ab4c29761bb0ffb31121de6cf2
williammian <william_mian@yahoo.com.br>
2019-11-12 21:35:19 -0200

    mostrando mensagens ao usuario (biblioteca toastr)

--- myfinances-app/src/views/login.js
+++ myfinances-app/src/views/login.js
@@ -5,13 +5,13 @@ import { withRouter } from 'react-router-dom'
 
 import UsuarioService from '../app/service/usuarioService'
 import LocalStorageService from '../app/service/localstorageService'
+import { mensagemErro } from '../components/toastr'
 
 class Login extends React.Component {
 
     state = {
         email: '',
-        senha: '',
-        mensagemErro: null
+        senha: ''
     }
 
     constructor() {
@@ -27,7 +27,7 @@ class Login extends React.Component {
             LocalStorageService.adicionarItem('_usuario_logado', response.data);
             this.props.history.push('/home');
         }).catch( erro => {
-            this.setState({mensagemErro: erro.response.data})
+            mensagemErro(erro.response.data);
         })
     }
 
@@ -42,9 +42,6 @@ class Login extends React.Component {
                 <div className="col-md-6" style={ {position: 'relative', left: '300px'} }>
                     <div className="bs-docs-section">
                         <Card title="Login">
-                            <div className="row">
-                                <span>{this.state.mensagemErro}</span>
-                            </div>
                             <div className="row">
                                 <div className="col-lg-12">
                                     <div className="bs-component">
