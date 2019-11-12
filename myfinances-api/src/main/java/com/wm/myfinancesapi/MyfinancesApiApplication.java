ea4e6be8e428ebe794265a61214f92283733c7a6
williammian <william_mian@yahoo.com.br>
2019-11-12 00:42:32 -0200

    habilitando CORS origin

--- myfinances-api/src/main/java/com/wm/myfinancesapi/MyfinancesApiApplication.java
+++ myfinances-api/src/main/java/com/wm/myfinancesapi/MyfinancesApiApplication.java
@@ -2,9 +2,18 @@ package com.wm.myfinancesapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class MyfinancesApiApplication {
+@EnableWebMvc
+public class MyfinancesApiApplication implements WebMvcConfigurer {
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyfinancesApiApplication.class, args);
