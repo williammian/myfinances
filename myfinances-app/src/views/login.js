ec5c0a95fee843857bbcffd026b756abc586390b
williammian <william_mian@yahoo.com.br>
2019-11-12 07:34:17 -0200

    manipulando dados vindos da API

--- myfinances-app/src/views/login.js
+++ myfinances-app/src/views/login.js
@@ -9,7 +9,8 @@ class Login extends React.Component {
 
     state = {
         email: '',
-        senha: ''
+        senha: '',
+        mensagemErro: null
     }
 
     entrar = () => {
@@ -18,9 +19,9 @@ class Login extends React.Component {
             email: this.state.email,
             senha: this.state.senha  
         }).then( response => {
-            console.log( response )
+            this.props.history.push('/home')
         }).catch( erro => {
-            console.log( erro.response )
+            this.setState({mensagemErro: erro.response.data})
         })
     }
 
@@ -35,6 +36,9 @@ class Login extends React.Component {
                 <div className="col-md-6" style={ {position: 'relative', left: '300px'} }>
                     <div className="bs-docs-section">
                         <Card title="Login">
+                            <div className="row">
+                                <span>{this.state.mensagemErro}</span>
+                            </div>
                             <div className="row">
                                 <div className="col-lg-12">
                                     <div className="bs-component">
