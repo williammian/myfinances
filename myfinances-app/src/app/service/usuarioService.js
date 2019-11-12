c8571ca7179933c38c6d4c2e9d78ac5d4e995d31
William Mian <william_mian@yahoo.com.br>
2019-11-12 13:11:37 -0300

    refatorando o componente da view Home

--- myfinances-app/src/app/service/usuarioService.js
+++ myfinances-app/src/app/service/usuarioService.js
@@ -10,6 +10,10 @@ class UsuarioService extends ApiService {
         return this.post('/autenticar', credenciais);
     }
 
+    obterSaldoPorUsuario(id) {
+        return this.get(`/${id}/saldo`);
+    }
+
 }
 
 export default UsuarioService;
--- myfinances-app/src/views/login.js
+++ myfinances-app/src/views/login.js
@@ -4,6 +4,7 @@ import FormGroup from '../components/form-group'
 import { withRouter } from 'react-router-dom'
 
 import UsuarioService from '../app/service/usuarioService'
+import LocalStorageService from '../app/service/localstorageService'
 
 class Login extends React.Component {
 
@@ -23,8 +24,8 @@ class Login extends React.Component {
             email: this.state.email,
             senha: this.state.senha
         }).then( response => {
-            localStorage.setItem('_usuario_logado', JSON.stringify(response.data) )
-            this.props.history.push('/home')
+            LocalStorageService.adicionarItem('_usuario_logado', response.data);
+            this.props.history.push('/home');
         }).catch( erro => {
             this.setState({mensagemErro: erro.response.data})
         })
