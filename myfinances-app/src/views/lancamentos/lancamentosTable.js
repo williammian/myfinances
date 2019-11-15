7d7dc2510f558672aa8244e25ce4f90e91882080
williammian <william_mian@yahoo.com.br>
2019-11-15 11:15:39 -0200

    melhorias view consulta de lancamentos

--- myfinances-app/src/views/lancamentos/lancamentosTable.js
+++ myfinances-app/src/views/lancamentos/lancamentosTable.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import currencyFormatter from 'currency-formatter'
 
 export default props => {
 
@@ -6,7 +7,7 @@ export default props => {
         return(
             <tr key={lancamento.id}>
                 <td>{lancamento.descricao}</td>
-                <td>{lancamento.valor}</td>
+                <td>{ currencyFormatter.format(lancamento.valor, { locale: 'pt-BR' })}</td>
                 <td>{lancamento.tipo}</td>
                 <td>{lancamento.mes}</td>
                 <td>{lancamento.status}</td>
