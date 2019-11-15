55bda1a9eae6a02a2216d0147a5f77310d8a051c
williammian <william_mian@yahoo.com.br>
2019-11-15 17:49:58 -0200

    instalando e testando a biblioteca PrimeReact no projeto

--- myfinances-app/src/main/App.js
+++ myfinances-app/src/main/App.js
@@ -3,11 +3,17 @@ import React from 'react';
 import Rotas from './rotas'
 import Navbar from '../components/navbar'
 
+import {Button} from 'primereact/button';
+
 import 'toastr/build/toastr.min.js'
 
 import 'bootswatch/dist/flatly/bootstrap.css'
 import '../custom.css'
 import 'toastr/build/toastr.css'
+
+import 'primereact/resources/themes/nova-light/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 
 class App extends React.Component {
 
@@ -15,6 +21,7 @@ class App extends React.Component {
     return (
       <>
         <Navbar />
+        <Button label="Click" icon="pi pi-check" iconPos="right" />
         <div className="container">
           <Rotas />
         </div>
