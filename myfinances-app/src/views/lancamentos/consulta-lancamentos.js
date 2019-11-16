2ce41a4a76d0097d934244aed2aa775605c47d35
williammian <william_mian@yahoo.com.br>
2019-11-16 10:52:39 -0200

    configurando a navegacao dos casos de uso de lancamentos

--- myfinances-app/src/views/lancamentos/consulta-lancamentos.js
+++ myfinances-app/src/views/lancamentos/consulta-lancamentos.js
@@ -82,6 +82,10 @@ class ConsultaLancamentos extends React.Component {
             });
     }
 
+    preparaFormularioCadastro = () => {
+        this.props.history.push('/cadastro-lancamentos')
+    }
+
     render(){
         const meses = this.service.obterListaMeses();
 
@@ -134,7 +138,7 @@ class ConsultaLancamentos extends React.Component {
                             </FormGroup>
 
                             <button onClick={this.buscar} type="button" className="btn btn-success">Buscar</button>
-                            <button type="button" className="btn btn-danger">Cadastrar</button>
+                            <button onClick={this.preparaFormularioCadastro} type="button" className="btn btn-danger">Cadastrar</button>
 
                         </div>
                     </div>
