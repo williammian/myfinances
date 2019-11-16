f6802b8576dcbd4bd74020fa989e2eea95814e73
williammian <william_mian@yahoo.com.br>
2019-11-16 17:25:18 -0200

    implementando o end-point de buscar o lancamento por id

--- myfinances-api/src/main/java/com/wm/myfinancesapi/api/resource/LancamentoResource.java
+++ myfinances-api/src/main/java/com/wm/myfinancesapi/api/resource/LancamentoResource.java
@@ -37,6 +37,13 @@ public class LancamentoResource {
 	
 	private final UsuarioService usuarioService;
 	
+	@GetMapping("{id}")
+	public ResponseEntity obterLancamento(@PathVariable("id") Long id) {
+		return service.obterPorId(id)
+					  .map(lancamento -> new ResponseEntity(converter(lancamento), HttpStatus.OK))
+					  .orElseGet( () -> new ResponseEntity(HttpStatus.NOT_FOUND));
+	}
+	
 	@PostMapping
 	@Transactional
 	public ResponseEntity salvar(@RequestBody LancamentoDTO dto) {
@@ -118,6 +125,19 @@ public class LancamentoResource {
 			}
 		}).orElseGet( () -> 
 			new ResponseEntity("Lançamento não encontrado na base de dados", HttpStatus.BAD_REQUEST));
+	}
+	
+	private LancamentoDTO converter(Lancamento lancamento) {
+		return LancamentoDTO.builder()
+				.id(lancamento.getId())
+				.descricao(lancamento.getDescricao())
+				.valor(lancamento.getValor())
+				.mes(lancamento.getMes())
+				.ano(lancamento.getAno())
+				.status(lancamento.getStatus().name())
+				.tipo(lancamento.getTipo().name())
+				.usuario(lancamento.getUsuario().getId())
+				.build();
 	}
 	
 	private Lancamento converter(LancamentoDTO dto) {
