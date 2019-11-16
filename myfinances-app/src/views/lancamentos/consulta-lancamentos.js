91aed9e152dc34fe780364c0ab3711b55bed4173
williammian <william_mian@yahoo.com.br>
2019-11-16 11:13:11 -0200

    passando parametros para o formulario atraves da rota

--- myfinances-app/src/views/lancamentos/consulta-lancamentos.js
+++ myfinances-app/src/views/lancamentos/consulta-lancamentos.js
@@ -56,7 +56,7 @@ class ConsultaLancamentos extends React.Component {
     }
 
     editar = (id) => {
-        console.log('editando o lançamento ', id);
+        this.props.history.push(`/cadastro-lancamentos/${id}`)
     }
 
     abrirConfirmacao = ( lancamento ) => {
