7cb5c3d18995cb1b5bef5db368b7f60deceee5aa
williammian <william_mian@yahoo.com.br>
2019-11-16 11:12:42 -0200

    passando parametros para o formulario atraves da rota

--- myfinances-app/src/views/lancamentos/cadastro-lancamentos.js
+++ myfinances-app/src/views/lancamentos/cadastro-lancamentos.js
@@ -26,6 +26,11 @@ class CadastroLancamentos extends React.Component {
         this.service = new LancamentoService();
     }
 
+    componentDidMount() {
+        const params = this.props.match.params;
+        console.log('params', params);
+    }
+
     submit = () => {
         const usuarioLogado = LocalStorageService.obterItem('_usuario_logado');
 
