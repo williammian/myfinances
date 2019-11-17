c3bbac8aeb8f27df82032e0ffcee54f7b05d9377
williammian <william_mian@yahoo.com.br>
2019-11-17 08:49:08 -0200

    eliminando a logica de validacao do componente de Cadastro de Usuario

--- myfinances-app/src/views/cadastroUsuario.js
+++ myfinances-app/src/views/cadastroUsuario.js
@@ -21,42 +21,18 @@ class CadastroUsuario extends React.Component {
         this.service = new UsuarioService();
     }
 
-    validar() {
-        const msgs = [];
-
-        if(!this.state.nome) {
-            msgs.push('O campo Nome é obrigatório.');
-        }
-
-        if(!this.state.email) {
-            msgs.push('O campo E-mail é obrigatório.');
-        }else if(!this.state.email.match(/^[a-z0-9.]+@[a-z0-9]+\.[a-z]/)) {
-            msgs.push('Informe um E-mail é válido.');
-        }
-
-        if(!this.state.senha || !this.state.senhaRepeticao) {
-            msgs.push('Digite a senha 2x.');
-        }else if(this.state.senha !== this.state.senhaRepeticao) {
-            msgs.push('As senhas não batem.');
-        }
-
-        return msgs;
-    }
-
     cadastrar = () => {
-        const msgs = this.validar();
 
-        if(msgs && msgs.length > 0) {
-            msgs.forEach( (msg, index) => {
-                mensagemErro(msg);
-            });
+        const { nome, email, senha, senhaRepeticao } = this.state;
+
+        const usuario = { nome, email, senha, senhaRepeticao };
+
+        try{
+            this.service.validar(usuario);
+        }catch(erro){
+            const msgs = erro.mensagens;
+            msgs.forEach(msg => mensagemErro(msg));
             return false;
-        }
-
-        const usuario = {
-            nome: this.state.nome,
-            email: this.state.email,
-            senha: this.state.senha
         }
 
         this.service.salvar(usuario)
