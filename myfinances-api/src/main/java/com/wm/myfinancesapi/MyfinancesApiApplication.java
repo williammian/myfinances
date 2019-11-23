20410d7f66a3729b5153384f1952f3d817c006d9
williammian <william_mian@yahoo.com.br>
2019-11-23 15:35:14 -0200

    separando configuracao Web CORS

--- myfinances-api/src/main/java/com/wm/myfinancesapi/MyfinancesApiApplication.java
+++ myfinances-api/src/main/java/com/wm/myfinancesapi/MyfinancesApiApplication.java
@@ -2,18 +2,10 @@ package com.wm.myfinancesapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@EnableWebMvc
 public class MyfinancesApiApplication implements WebMvcConfigurer {
-	
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyfinancesApiApplication.class, args);
